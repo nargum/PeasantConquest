d0de0322f295787108cc6457ecea34b889c6effe
typekcz <lkotrzena@gmail.com>
2017-10-12 16:26:10 +0200

    Tiled tileset and map

---
+++ app/src/main/res/xml/tiles.tsx
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="PeasantConquest" tilewidth="64" tileheight="64" tilecount="17" columns="0">
+ <tile id="0">
+  <properties>
+   <property name="resource" value="tile_grass1"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_grass1.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="resource" value="tile_grass2"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_grass2.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="resource" value="tile_road_e"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_e.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="resource" value="tile_road_n"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_n.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="resource" value="tile_road_ne"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_ne.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="resource" value="tile_road_ns"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_ns.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="resource" value="tile_road_nse"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_nse.png"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="resource" value="tile_road_nsw"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_nsw.png"/>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="resource" value="tile_road_nswe"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_nswe.png"/>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="resource" value="tile_road_nw"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_nw.png"/>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="resource" value="tile_road_nwe"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_nwe.png"/>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="resource" value="tile_road_s"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_s.png"/>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="resource" value="tile_road_se"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_se.png"/>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="resource" value="tile_road_sw"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_sw.png"/>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="resource" value="tile_road_swe"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_swe.png"/>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="resource" value="tile_road_w"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_w.png"/>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="resource" value="tile_road_we"/>
+  </properties>
+  <image width="64" height="64" source="../drawable-nodpi/tile_road_we.png"/>
+ </tile>
+</tileset>
